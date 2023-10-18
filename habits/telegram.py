aa2e713f219abfd340cce66cef06bd1b2574742b
Aleksandr888008 <learningenglish2804@gmail.com>
2023-10-18 15:07:56 +0400

    add

--- habits/telegram.py
+++ habits/telegram.py
@@ -6,6 +6,7 @@ TOKEN = settings.TELEGRAM_TOKEN
 
 
 def send_message(user, text):
+    """Подготовка для отправки сообщения в телеграм"""
     user_chat_id = user.chat_id
     response = requests.get(
         f"https://api.telegram.org/bot{TOKEN}/sendMessage", {
@@ -14,3 +15,13 @@ def send_message(user, text):
         }
     )
     return response.json()
+
+
+def update_chat_id():
+    """Получение chat_id пользователей"""
+    response = requests.get(f"https://api.telegram.org/bot{TOKEN}/getUpdates").json()
+    if response.get("ok"):
+        user_chats = []  # Получение списка
+        for ch_id in response.get("result"):
+            chat_id = ch_id.get("message").get("chat").get("id")
+            user_chats.append(chat_id)
